a80f5b11b43b9110ed4587053cef85c480a9d5b1
rushivachhani <rushi.b.vachhani@gmail.com>
2020-04-17 17:01:31 +0530

    third commit(Rushi) welcomepanel userclass+pojo

--- Parikshav1/Form1.cs
+++ Parikshav1/Form1.cs
@@ -14,6 +14,7 @@ namespace Parikshav1
 {
     public partial class Form1 : Form
     {
+        User currentLoginingUser;
         public Form1()
         {
             InitializeComponent();
@@ -28,10 +29,10 @@ namespace Parikshav1
 
         private void lbLogin_Click(object sender, EventArgs e)
         {
-            User currentLoginingUser = new User();
+            
             var username = tbUsername.Text;
             var password = tbPassword.Text;
-            currentLoginingUser.getUser(username, password);
+            User.getUser(username, password);
         }
         private void button1_Click(object sender, EventArgs e)
         {
--- Parikshav1/Program.cs
+++ Parikshav1/Program.cs
@@ -23,7 +23,7 @@ namespace Parikshav1
     }
     class User
     {
-        User currentUser = null;
+        
         bool loginStatus = false;
         String userName;
         String useremail;
@@ -31,35 +31,61 @@ namespace Parikshav1
         String userType;
         String userPassword;
 
-        
-        public User getUser(String email, String Pass)
+        public string UserName
         {
+            get { return userName; } 
+            set { userName = value; }
+        }
+        public string Useremail
+        {
+            get { return useremail; }
+            set { useremail = value; }
+        }
+        public string UserLastLogin
+        {
+            get { return userLastLogin; }
+            set { userLastLogin = value; }
+        }
+        public string UserType
+        {
+            get { return userType; }
+            set { userType = value; }
+        }
+        public string UserPassword
+        {
+            get { return userPassword; }
+            set { userPassword = value; }
+        }
+
+        public static User getUser(String email, String Pass)
+        {
+            User currentUser = new User();
             string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
             MySqlConnection con = new MySqlConnection(ConString);
             con.Open();
             var cmd = new MySqlCommand();
             cmd.Connection = con;
             cmd.CommandText = "select * from tb_student";
-
+            Console.WriteLine("Db working");
             MySqlDataReader dataReader = cmd.ExecuteReader();
 
             var username = email;
             var password = Pass;
-
+            Console.WriteLine("Db working");
             dataReader.Read();
             if (username.Equals(dataReader.GetString("UserEmail")) && password.Equals(dataReader.GetString("UserPassword")))
             {
+                Console.WriteLine("Db working");
                 currentUser.userName = dataReader.GetString("UserName");
                 currentUser.useremail = dataReader.GetString("UserEmail");
                 currentUser.loginStatus = true;
                 currentUser.userLastLogin = dataReader.GetString("UserLastLogin");
                 currentUser.userType = dataReader.GetString("UserType");
+                con.Close();
                 return currentUser;
             }
-
+            con.Close();
             return null;
         }
-        
-
     }
 }
