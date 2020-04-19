d22888213f59ec308afab733abb114cc41be0bef
vivekshah1801 <vivekshah9969@gmail.com>
2020-04-19 13:40:56 +0530

    viveknewbranch

---
+++ demov1/POJOs/Question.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demov1.POJOs
+{
+    class Question
+    {
+        public int id { get; set; }
+        public string q { get; set; }
+
+        public string op1 { get; set; }
+        public string op2 { get; set; }
+        public string op3 { get; set; }
+        public string op4 { get; set; }
+        public string opCorrect { get; set; }
+
+        public int eid { get; set; }
+
+        Question(string q,string op1, string op2, string op3, string op4, string opCorrect,int eid)
+        {
+            this.q = q;
+            this.op1 = op1;
+            this.op2 = op2;
+            this.op3 = op3;
+            this.op4 = op4;
+            this.opCorrect = opCorrect;
+            this.eid = eid;
+        }
+        
+        Question()
+        {
+
+        }
+
+
+    }
+}
--- demov1/Program.cs
+++ demov1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace demov1
 {
     static class Program
@@ -16,7 +17,7 @@ namespace demov1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WelcomeForm());
         }
     }
 }
